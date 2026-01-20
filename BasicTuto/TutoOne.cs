30ef0880ee663ff989697cfe00c76ff9f52c51be
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-20 22:16:32 +0100

    NexusCSharp BasicTuto TutoOne.cs. ExploreOrderOfOperations

--- BasicTuto/TutoOne.cs
+++ BasicTuto/TutoOne.cs
@@ -30,3 +30,25 @@ void WorkWithIntegers()
     c = a + b - 12 * 17; // mixing variables and constant numbers
     Console.WriteLine(c);
 }
+
+/*  ### 2.Explore order of operations ###*/
+// the rules you learned in mathematics
+OrderPrecedence();
+
+void OrderPrecedence()
+{ 
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c; // the multiplication is performed before the addition
+    Console.WriteLine(d);
+    d = (a + b) * c; // force a different order of operation by adding parentheses
+    Console.WriteLine(d);
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12; // combining many different operations
+    Console.WriteLine(d);
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g; // Integer dividion always produces an integer result
+    Console.WriteLine(h);
+}
