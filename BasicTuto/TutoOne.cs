23e0281d6e565ecd496fa5e53a676b8cfa91545d
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-20 21:52:41 +0100

    NexusCSharp BasicTuto TutoOne.cs. Integers

--- BasicTuto/TutoOne.cs
+++ BasicTuto/TutoOne.cs
@@ -9,3 +9,24 @@
     4. Work with the double type.
     5. Work with decimal types.
 */
+
+
+/*  ### 1.Explore integer math ###*/
+// separate method
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b; // addition
+    Console.WriteLine(c);
+    c = a - b; // subtraction
+    Console.WriteLine(c);
+    c = a * b; // multiplication
+    Console.WriteLine(c);
+    c = a / b; // division
+    Console.WriteLine(c);
+    c = a + b - 12 * 17; // mixing variables and constant numbers
+    Console.WriteLine(c);
+}
