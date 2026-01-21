a5e0a0dda0240eb2341847afa7bca2c1fc1510be
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-21 20:31:34 +0100

    NexusCSharp BasicTuto Tuples

---
+++ BasicTuto/Tuples.cs
@@ -0,0 +1,9 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: Create types in C#.
+    1. Tuples.
+    2. Create record types.
+    3. Struct, class, and interface types.
+*/
