ed505062239cb6fd577d3b09cbe9d391c3a8fb85
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-22 23:37:12 +0100

    NexusCSharp BasicTuto BranchesLoops 2.make if and else work together

--- BasicTuto/BranchesLoops.cs
+++ BasicTuto/BranchesLoops.cs
@@ -24,3 +24,82 @@ if (a + b > 10) // nothing is printed. condition is false
 
 
 /* ### 2.Make if and else work together ###*/
+int a = 5;
+int b = 3;
+if (a + b > 10)
+    Console.WriteLine("The answer is greater than 10.");
+else // the statement following the else keyword executes only when the condition being tested is false.
+    Console.WriteLine("The answer is not greater than 10.");
+int a = 5;
+int b = 3;
+if (a + b > 10)
+{
+    Console.WriteLine("The answer is greater than 10.");
+}
+else 
+{
+    Console.WriteLine("The answer is not greater than 10.");
+}// when you want more than one statement to be part of the block that executes conditionally. {}
+
+int a = 5;
+int b = 5;
+int c = -3;
+if ((a + b + c > 10) && (a == b)) // "==" symbol tests for equality. "&&" represents "and".
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not equal to the second");
+}
+int a = 5;
+int b = 5;
+int c = -3;
+if ((a + b + c > 10) || (a == b)) // "||" or.
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not equal to the second");
+}
+
+ExploreIf();
+void ExploreIf()
+{
+int a = 5;
+int b = 3;
+if (a + b > 10)
+{
+    Console.WriteLine("The answer is greater than 10");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+}
+int c = 4;
+if ((a + b + c > 10) && (a > b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is greater than the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not greater than the second");
+}
+if ((a + b + c > 10) || (a > b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("Or the first number is greater than the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("And the first number is not greater than the second");
+}
+}
