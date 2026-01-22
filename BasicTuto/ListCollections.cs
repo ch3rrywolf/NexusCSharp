72cbd53120c89ba49f1a07bac25938c192e95f31
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-23 00:23:44 +0100

    NexusCSharp BasicTuto ListCollections

---
+++ BasicTuto/ListCollections.cs
@@ -0,0 +1,11 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: Learn to manage data collections using List<T> in C#.
+    1. A basic list example.
+    2. Modify list contents.
+    3. Search and sort lists.
+    4. Lists of other types.
+    5. Challenge.
+*/
