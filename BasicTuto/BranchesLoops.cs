07e594689d00be3c30df803043b75faa4ceb1387
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-22 21:59:28 +0100

    NexusCSharp BasicTuto BranchesLoops 1.use if statements

--- BasicTuto/BranchesLoops.cs
+++ BasicTuto/BranchesLoops.cs
@@ -10,3 +10,17 @@
     5. Created nested loops.
     6. Combine branches and loops.
 */
+
+
+/* ### 1.Use if statements ###*/
+// The if statement checks the value of a bool.
+// When the value is true, the statement following the if executes. 
+// this process of checking conditions and executing statements based on those conditions is powerful.
+int a = 5;
+int b = 6;
+b = 3;
+if (a + b > 10) // nothing is printed. condition is false
+    Console.WriteLine("The answer is greater than 10.");
+
+
+/* ### 2.Make if and else work together ###*/
