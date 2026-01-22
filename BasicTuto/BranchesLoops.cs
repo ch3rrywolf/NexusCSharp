b10e4538c868662f96cb02f75077e04445f852a9
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-22 21:13:41 +0100

    NexusCSharp BasicTuto BranchesLoops

---
+++ BasicTuto/BranchesLoops.cs
@@ -0,0 +1,12 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: C# if statements and loops conditional logic.
+    1. Use if statements.
+    2. Make if and else work together.
+    3. Use loops to repeat operations.
+    4. Work with the for loop.
+    5. Created nested loops.
+    6. Combine branches and loops.
+*/
