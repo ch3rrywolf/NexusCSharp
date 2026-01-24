33f0436e48857e121a2c1e5d5d2f3277be9cceae
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-24 23:00:58 +0100

    NexusCSharp FundamentalsTuto Overview.cs 1.General Structure of a C# Program

---
+++ FundamentalsTuto/ProgramStructure/Overview.cs
@@ -0,0 +1,73 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: C# program structure.
+    1. General Structure of a C# Program.
+    2. Building and running C# programs.
+    3. Expressions and statements.
+*/
+
+
+/* ### 1. General Structure of a C# Program. ###*/
+// The preceding example uses top-level statements for the program's entry point.
+// Only one file can have top-level statements.
+using System;
+Console.WriteLine("Hello world!");
+namespace YourNamespace
+{
+class YourClass
+{
+}
+struct YourStruct
+{
+}
+interface IYourInterface
+{
+}
+delegate int YourDelegate();
+enum YourEnum
+{
+}
+namespace YourNestedNamespace
+{
+struct YourStruct
+{
+}
+}
+}
+
+// A skeleton of a C# program
+// In that case the program starts in the opening brace of Main method.
+using System;
+namespace YourNamespace
+{
+class YourClass
+{
+}
+struct YourStruct
+{
+}
+interface IYourInterface
+{
+}
+delegate int YourDelegate();
+enum YourEnum
+{
+}
+namespace YourNestedNamespace
+{
+struct YourStruct
+{
+}
+}
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Hello world!");
+}
+}
+}
+
+/* ### 2. Building and running C# programs. ###*/
