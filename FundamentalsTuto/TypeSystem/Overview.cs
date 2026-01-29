6e0d504f863302472c59c863cb079ffec1d2a100
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-01-30 00:57:55 +0100

    NexusCSharp FundamentalsTuto TypeSystem Overview.cs

---
+++ FundamentalsTuto/TypeSystem/Overview.cs
@@ -0,0 +1,18 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: The C# type system.
+    1. Specifying types in variable declarations.
+    2. Built-in types.
+    3. Custom types.
+    4. The common type system.
+    5. Value types.
+    6. Reference types.
+    7. Types of literal values.
+    8. Generic types.
+    9. Tuples and anonymous types.
+    10. Nullable value types.
+    11. Implicit type declarations.
+    12. Compile-time type and run-time type.
+*/
