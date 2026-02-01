7f4498633641ca60f0096d1fc1e4c37afa3f5048
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-02-01 10:01:36 +0100

    NexusCSharp FundamentalsTuto TypeSystem Namespaces.cs

---
+++ FundamentalsTuto/TypeSystem/Namespaces.cs
@@ -0,0 +1,7 @@
+/*
+    Basic CSharp (C#) learn with @ch3rrywolf.exe
+    ##### ###### #### ##### #### ###############
+
+   Tutorial: Declare namespaces to organize types.
+    1. Namespaces overview.
+*/
