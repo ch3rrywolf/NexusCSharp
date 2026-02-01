36bc75cc78ea218e2d80a9fba065b53223c518c4
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-02-01 08:27:25 +0100

    NexusCSharp FundamentalsTuto TypeSystem Overview.cs 1.Specifying types in variable declarations 2.Built-in types

--- FundamentalsTuto/TypeSystem/Overview.cs
+++ FundamentalsTuto/TypeSystem/Overview.cs
@@ -16,3 +16,41 @@
     11. Implicit type declarations.
     12. Compile-time type and run-time type.
 */
+
+
+// If you declare a variable of type int,
+// the compiler allows you to use the variable in addition and subtraction operations.
+// If you try to perform those same operations on a variable of type bool,
+// the compiler generates an error.
+int a = 5;
+int b = a + 2; //OK
+bool test = true;
+// Error. Operator '+' cannot be applied to operands of type 'int' and 'bool'.
+int c = a + test;
+// bool isn't convertible to int.
+
+
+/* ### 1. Specifying types in variable declarations. ###*/
+// When you declare a variable or constant in a program,
+// you must either specify its type or use the var keyword to let the compiler infer the type.
+// Declaration only:
+float temperature;
+string name;
+MyClass myClass;
+// Declaration with initializers (four examples):
+char firstLetter = 'C';
+var limit = 3;
+int[] source = [0, 1, 2, 3, 4, 5];
+var query = from item in source
+            where item <= limit
+            select item;
+// After you declare a variable, you can't redeclare it with a new type, and you can't assign a value that's incompatible with its declared type.
+// For example, you can't declare an int and then assign it a Boolean value of true .
+// However, you can convert values to other types, such aswhen you assign them to new variables or pass them as method arguments.
+// The compilerautomatically performs a type conversion that doesn't cause data loss.
+// A conversion that might cause data loss requires a cast in the source code.
+
+
+/* ### 2. Built-in types. ###*/
+// hese types represent integers, floating point values, Boolean expressions, text characters, decimal values, and other types of data.
+// The language also includes built-in string and object types.
