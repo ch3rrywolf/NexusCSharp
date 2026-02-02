5568fb6b80206212a396ac1c570674c89db3b1aa
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-02-02 14:11:29 +0100

    NexusCSharp Exercises Easy DataTypesA CalculateDimensionsOfSphere.cs 1/8

--- Exercises/Easy/DataTypesA/CalculateDimensionsOfSphere.cs
+++ Exercises/Easy/DataTypesA/CalculateDimensionsOfSphere.cs
@@ -13,4 +13,20 @@
         Output:
           surface = 45238,93
           volume = 678584,1
-*/
+*/ 
+
+using System;
+
+public class CalculateDimensionsOfSphere
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("given its radius");
+    float radius = Convert.ToSingle(Console.ReadLine());
+    float pi = 3.14159f;
+    float surface = 4 * pi * (radius * radius);
+    float volume = 4 / 3 * pi * (radius * radius * radius);
+    Console.WriteLine($"surface = {surface}");
+    Console.WriteLine($"volume = {volume}");
+  }
+}
