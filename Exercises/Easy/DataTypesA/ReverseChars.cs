f0befd02352e0a9a1fc6a51e00b407cd9c6c09a5
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-02-02 14:48:01 +0100

    NexusCSharp Exercises Easy DataTypesA ReverseChars.cs 3/8

--- Exercises/Easy/DataTypesA/ReverseChars.cs
+++ Exercises/Easy/DataTypesA/ReverseChars.cs
@@ -4,4 +4,33 @@
 
     Description: Reverse chars.
     - Write a program to ask the user for three letters and display them in reverse order.
+
+    - Example:
+        Input:
+          a
+          b
+          c
+        Output:
+            c, b ,a
 */
+
+using System;
+
+public class ReverseChars
+{
+    public static void Main(string[] args)
+    {
+        char a = Convert.ToChar(Console.ReadLine());
+        char b = Convert.ToChar(Console.ReadLine());
+        char c = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine($"{c}, {b}, {a}");
+    }
+}
+
+// Note: warning CS8604: Possible null reference argument for parameter 'value' in 'char Convert.ToChar(string value)'
+// Convert.ToChar(Console.ReadLine()!)
+// or
+// Console.ReadKey().KeyChar
+// or
+// char.Parse(Console.ReadLine() ?? "")
