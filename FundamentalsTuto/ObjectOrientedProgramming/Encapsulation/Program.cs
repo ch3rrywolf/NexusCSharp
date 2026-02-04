374870fd319611f910fa475f1eb595b881fa6ecb
ch3rrywolf <ch3rrywolf111@gmail.com>
2026-02-04 11:11:46 +0100

    NexusCSharp FundamentalsTuto ObjectOrientedProgramming Encapsulation

---
+++ FundamentalsTuto/ObjectOrientedProgramming/Encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Encapsulation;
+
+var employee = new Employee();
+employee.SetName("ch3rry", "wolf");
+employee.SetBirthDate(new DateOnly(1997, 1, 1));
+employee.SetBasicSalary(2000);
+employee.SetTaxPercentage(20);
+
+Console.WriteLine($"Full Name:\t {employee.FirstName} {employee.LastName}");
+Console.WriteLine($"Birth Date:\t {employee.BirthDate}");
+Console.WriteLine($"Basic Salary:\t {employee.BasicSalary:0.00}");
+Console.WriteLine($"Tax Percentage:\t {employee.TaxPercentage} %");
+Console.ReadKey();
